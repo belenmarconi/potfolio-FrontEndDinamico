a8883c0dd9424128da2ff0c609c0268b394acba8
belenmarconi <belenmarconi@gmail.com>
2023-01-06 00:40:52 -0300

    2

--- desarrollo-frontend/src/app/app.component.ts
+++ desarrollo-frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'desarrollo-frontend';
+  title = 'PORTFOLIO Belén Marconi';
 }
